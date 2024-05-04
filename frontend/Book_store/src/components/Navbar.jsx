6f16be8c4427f63b5f3330602b93a785546db11d
Adarsh754 <adarshbhagat459@gmail.com>
2024-05-04 21:04:05 +0530

    commit

--- frontend/Book_store/src/components/Navbar.jsx
+++ frontend/Book_store/src/components/Navbar.jsx
@@ -47,7 +47,7 @@ function Navbar() {
     )
     return (
         <>
-            <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-white text-black fixed top-0 left-0 right-0 z-50 ${
+            <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-white dark:text-black fixed top-0 left-0 right-0 z-50 ${
                 sticky
                 ?"sticky-navbar shadow-md bg-base-200 duration-300  transition-all ease-in-out":"" 
             }`}>
