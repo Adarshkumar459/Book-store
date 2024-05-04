591d84ad2a396394f548a9fe763b9bab2d82e839
Adarsh754 <adarshbhagat459@gmail.com>
2024-05-04 10:28:16 +0530

    remove

--- Backend/c program/cube.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    a =5**2;
-    b= 5**3;
-    printf("%d\n",a);
-     printf("%d\n",b);
-    return 0;
-}
--- Backend/c program/stringarray.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    char ch[20.]="bhopal city";
-   // printf("%c%c%c%c%c%c",ch[0],ch[1],ch[2],ch[3],ch[4],ch[5]);
-   printf("%s",ch);
-   
-}
