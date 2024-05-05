7cfe7655328d8990f397c2f5844a8c87d104024a
Adarsh754 <adarshbhagat459@gmail.com>
2024-05-05 13:09:20 +0530

    frontend complete

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,36 +1,58 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { useForm } from "react-hook-form"
 
 function Login() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm()
+      const onSubmit= (data) => console.log(data)
+
   return (
     <div>
         <dialog id="my_modal_3" className="modal">
             <div className="modal-box dark:bg-white dark:text-black">
-                <form method="dialog">
+                <form onSubmit={handleSubmit(onSubmit)} method="dialog">
                     {/* if there is a button in form, it will close the modal */}
-                    <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-                     onClick={()=> document.getElementById("my_modal_3").close()}>✕</Link>
+                    <Link to="/" 
+                        className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                        onClick={()=> document.getElementById("my_modal_3").close()}>✕
+                     </Link>
                     
-                </form>
-                <h3 className="font-bold text-lg">Login</h3>
-                {/* Email Field */}
-                <div className='mt-4 space-y-2'>
-                    <span>Email</span>
-                    <br />
-                    <input type="email" placeholder='Enter your Email' className='w-80 px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black' />
-                </div>
-                {/* Password field */}
-                <div className='mt-4 space-y-2'>
-                    <span>Password</span>
-                    <br /> 
-                    <input type="text" placeholder='Enter your Password' className='w-80 px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black' />
-                </div>
-                {/* button saection  */}
-                <div className='flex justify-around mt-4'>
-                    <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200'>Login</button>
-                    <p>Not registered <Link to={"/signup"} className=' underline text-blue-600 cursor-pointer'>Singup</Link></p>
-                </div>
+               
+                    <h3 className="font-bold text-lg">Login</h3>
+                    {/* Email Field */}
+                    <div className='mt-4 space-y-2'>
+                        <span>Email</span>
+                        <br />
+                        <input 
+                        type="email" 
+                        placeholder='Enter your Email' 
+                        className='w-80 px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black'
+                        {...register("email", { required: true })} />
+                    </div>
+                    {errors.email && <span  className=' text-sm text-red-500'>email is required </span>}
+
+                    {/* Password field */}
+                    <div className='mt-4 space-y-2'>
+                        <span>Password</span>
+                        <br /> 
+                        <input 
+                         type="text"
+                         placeholder='Enter your Password'
+                         className='w-80 px-3 py-1 border rounded-md outline-none dark:bg-white dark:text-black'
+                         {...register("password", { required: true })} />
+                    </div>
+                    {errors.password && <span className=' text-sm text-red-500'>Password is require</span>}
+
+                    {/* button saection  */}
+                    <div className='flex justify-around mt-4'>
+                        <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200'>Login</button>
+                        <p>Not registered <Link to={"/signup"} className=' underline text-blue-600 cursor-pointer'>Singup</Link></p>
+                    </div>
+                </form>    
             </div>
         </dialog>
     </div>
